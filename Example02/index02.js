2ffbbb4499e0368dc78cc0520a614e3d641d53ae
Suleman Soudagar <sulemans@kpit.com>
2018-10-27 15:28:20 +0530

    added authentication login in authentication function

--- Example02/index02.js
+++ Example02/index02.js
@@ -2,19 +2,31 @@ const employees = require("./employees");
 
 const departments = require("./departments");
 
-const authenticate = function(email)
+const authenticate = function(email,password)
 {
     return new Promise(function(resolve,reject){
+        let status = false;
+
+        if(employees.has(email))
+            {
+                let employee = employees.get(email);
+                if(employee.password == password)
+                {
+                    status = true;            
+                }
+            }
+
         setTimeout(function(){
-            resolve({data:"This is an example"});
+            if(status)
+                resolve({result:status});
+            else
+                reject("Invalid email or password");
         },100);
     });
 }
 
-authenticate("sachin").then(function(result){
+authenticate("sachin@gmail.com","pass@123").then(function(result){
     console.log(result);
 }).catch(function(error){
-    Console.log(error);
-})
-
-
+    console.log(error);
+});
