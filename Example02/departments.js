9db04268aa8205f3dc381a86042200639f601b15
Suleman Soudagar <sulemans@kpit.com>
2018-10-27 14:14:55 +0530

    added example02

---
+++ Example02/departments.js
@@ -0,0 +1,13 @@
+let departments = new Map();
+
+departments.set("DT",
+    {
+        departmentName:"DT",location:"Pune",manager:"Ramesh K"
+    });
+
+departments.set("IES",
+    {
+        departmentName:"IES",location:"Mumbai",manager:"Kunthsam K"
+    });
+
+module.exports = departments;
---
+++ Example02/employees.js
@@ -0,0 +1,18 @@
+
+let employees = new Map();
+
+employees.set("sachin@gmail.com",
+        {
+            email:"sachin@gmail.com",password:"pass@123",
+            name:"sachin gupta",
+            age:23,salary:4000,phone:"9090",department:"DT"
+        });
+
+employees.set("ramesh@gmail.com",
+        {
+            email:"ramesh@gmail.com",password:"pass@321",
+            name:"ramesh gupta",
+            age:23,salary:4000,phone:"9091",department:"DT"
+        });
+
+module.exports = employees;
