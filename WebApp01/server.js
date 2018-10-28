cf6e6fbfd6ee2b958d97660d6b466e637e973107
Suleman Soudagar <sulemans@kpit.com>
2018-10-28 11:48:34 +0530

    added authentication logic

--- WebApp01/server.js
+++ WebApp01/server.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const fs = require("fs");
 
+const URL = require("url");
+
 let server = http.createServer(function(request,response){
     
     let url = request.url;
@@ -26,9 +28,28 @@ let server = http.createServer(function(request,response){
         });  
 
         break;
-    default :
+        /*default :
         response.writeHead(404,{'Content-Type':'text/html'});
         response.write("<h1>PAGE NOT FOUND : </h1>");
+        response.end();
+        */
+    }
+
+    if(url.startsWith("/authenticate"))
+    {
+        response.writeHead(200,{'Content-Type':'text/html'});
+        let querystring = URL.parse(url,true);
+        let email = querystring.query.email;
+        let password = querystring.query.password;
+
+        if(email == password && email.length > 5)
+        {
+            response.write("<h3>Login Successful</h3>");
+        }
+        else
+        {
+            response.write("<h3>Login Failed</h3>");
+        }
         response.end();
     }
 
